472c81340f9362b7ca7ad62070f7a0e1c9cd4102
Samba Diallo <sambadiallo@MacBook-Pro-de-Sambabhouria.local>
2024-05-03 17:25:35 +0200

    Gym Landing Page - React + TypeScript + Vite

---
+++ src/components/atoms/InputField.tsx
@@ -0,0 +1,8 @@
+type InputFieldProps = {
+  className: string
+  children: React.ReactNode
+}
+
+export const InputField = ({ className, children }: InputFieldProps) => {
+  return <div className={className}>{children}</div>
+}
